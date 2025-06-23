613f82d751745ff516db70fa25e510cb4da31f3a
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-06-23 17:11:15 -0300

    :sparkles: Criação das Páginas de Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import Footer from "./components/footer/Footer"
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 	return (
 		<>
-			<Navbar />
-			<Home />
-			<Footer />
+			<AuthProvider>
+				<BrowserRouter>
+					<Navbar />
+					<div className="min-h-[80vh]">
+						<Routes>
+							<Route
+								path="/"
+								element={<Login />}
+							/>
+							<Route
+								path="/home"
+								element={<Home />}
+							/>
+							<Route
+								path="/cadastro"
+								element={<Cadastro />}
+							/>
+							<Route
+								path="/login"
+								element={<Login />}
+							/>
+						</Routes>
+					</div>
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>
 		</>
 	)
 }
