c59d3bf90f2eabae099465385ec570764657e578
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-06-25 12:24:40 -0300

    :sparkles: CRUD de Tema

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormTema from './components/temas/formtema/FormTema'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
 
 function App() {
 	return (
@@ -14,22 +17,14 @@ function App() {
 					<Navbar />
 					<div className="min-h-[80vh]">
 						<Routes>
-							<Route
-								path="/"
-								element={<Login />}
-							/>
-							<Route
-								path="/home"
-								element={<Home />}
-							/>
-							<Route
-								path="/cadastro"
-								element={<Cadastro />}
-							/>
-							<Route
-								path="/login"
-								element={<Login />}
-							/>
+							<Route	path="/" element={<Login />} />
+							<Route	path="/home" element={<Home />}	/>
+							<Route	path="/cadastro" element={<Cadastro />}	/>
+							<Route path="/login" element={<Login />} />
+							<Route path="/temas" element={<ListaTemas />} />
+							<Route path="/cadastrartema" element={<FormTema />} />
+							<Route path="/editartema/:id" element={<FormTema />} />
+							<Route path="/deletartema/:id" element={<DeletarTema />} />
 						</Routes>
 					</div>
 					<Footer />
